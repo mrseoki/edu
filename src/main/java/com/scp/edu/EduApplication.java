8d94ab7fd88d45484955c5f76a97bd8117e9282d
mrseoki <mrseoki@naver.com>
2021-06-16 17:35:47 +0900

    소스변경

--- src/main/java/com/scp/edu/EduApplication.java
+++ src/main/java/com/scp/edu/EduApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EduApplication {
 
 	public static void main(String[] args) {
+		//주석
 		SpringApplication.run(EduApplication.class, args);
 	}
 
